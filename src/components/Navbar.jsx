56b46d93ae1750bb74ccce99311a82c84e822919
anan19940920-bit <anan19940920@gmail.com>
2026-01-31 11:42:37 +0800

    feat:新增css+jsx程式碼
    把大致的架構先寫出來，部分需要import其他檔案，如：MainLogo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,45 @@
-abc
+import './Navbar.css';
+import Logo from '.mainLogo';
+
+const menuItem = [
+    {
+        id:1,
+        title:'品牌故事',
+    },{
+        id:2,
+        title:'商品列表',
+    },{
+        id:3,
+        title:'咖啡文章',
+    },{
+        id:4,
+        title:'人格測驗',
+    }
+];
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/*要匯入logo */}
+            <MainLogo />
+            {/*選單按鈕區塊*/}
+            <ul className="navbar-menu">
+                {menuItem.map(({id,title}) =>(
+                    <li key={id} className="nav-item">
+                        {title}
+                    </li>
+                ))}
+            </ul>
+            {/*icon的區塊 */}
+            <div className="navbar-icons">
+                <span>search</span>
+                <span>carts</span>
+                <span>person</span>
+            </div>
+        </nav>
+    );
+};
+
+
+export default Navbar;
+
