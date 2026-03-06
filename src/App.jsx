9ff30db8d22002b706856832221a444d857a6d8d
anan19940920-bit <anan19940920@gmail.com>
2026-03-06 17:44:25 +0800

    feat: 串接產品詳情頁至 App 入口並配置環境樣式
    更新及調整程式碼內容，然後index.css會影響我的切版，所以我全部註解掉

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProductDetail from './ProductDetail'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="my-work-section" style={{ borderBottom: '2px solid #ccc', marginBottom: '20px' }}>
+        <ProductDetail />
+      </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
